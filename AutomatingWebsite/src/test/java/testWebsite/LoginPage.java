1d30710eb5d551affe70662496eaa7cb39f8cb15
kushm <kushma.nuguri@gmail.com>
2025-03-27 14:50:54 +0530

    Initial commit

---
+++ AutomatingWebsite/src/test/java/testWebsite/LoginPage.java
@@ -0,0 +1,33 @@
+package testWebsite;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginPage {
+	
+	protected WebDriver driver;
+	
+	@Test
+	public void TestWebsite() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.get("https://thinking-tester-contact-list.herokuapp.com/");
+		driver.findElement(By.id("email")).sendKeys("kushma.nuguri@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("praveenm");
+		driver.findElement(By.id("submit")).click();
+		System.out.println(driver.getTitle());
+		Thread.sleep(3000);
+		driver.close();
+	}
+
+	
+	
+	
+
+}
